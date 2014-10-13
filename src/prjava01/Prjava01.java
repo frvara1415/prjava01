13f46cc8ddae841f93759c529e343e5c80f7a3f5
francesc <francesc@francesc-Lenovo-G500s>
2014-10-13 16:24:48 +0200

    versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package prjava01;
 
 /**
@@ -23,15 +19,14 @@ public class Prjava01 {
             bw.write("<html>");bw.newLine(); 
             bw.write("  <head>");bw.newLine(); 
             bw.write("    <title>");bw.newLine();
-            bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
             bw.write("      Nova p&agrave;gina  web");bw.newLine(); 
             bw.write("    </title>");bw.newLine(); 
             bw.write("  </head>");bw.newLine(); 
             bw.write("  <body>");bw.newLine(); 
-            bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine(); 
+
             bw.write("    Nova p&agrave;gina web");bw.newLine(); 
             bw.write("  </body>");bw.newLine(); 
-            bw.write("</html>");bw.newLine(); 
+            bw.write("</html>");bw.newLine();
             bw.close(); 
         } 
     } 
